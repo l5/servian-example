2efa173dcb808a38c61ff4dc7ec5fc93a0c16a21
Dan <dan@ahawildlifebakery.com>
2022-03-13 17:08:57 +1300

    Added autoscaling

--- servian-example.tf
+++ servian-example.tf
@@ -234,3 +234,55 @@ resource "azurerm_network_profile" "servian-vnet" {
     }
   }
 }
+
+/* Autoscaling */
+resource "azurerm_monitor_autoscale_setting" "servian" {
+  name                = "servianAutoscaleSetting"
+  resource_group_name = azurerm_resource_group.servian.name
+  location            = azurerm_resource_group.servian.location
+  target_resource_id  = azurerm_app_service_plan.servian.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.servian.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.servian.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }  
+}
